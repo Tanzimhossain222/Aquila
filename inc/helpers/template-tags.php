b306d858b70466a9c6797d90067935f517761fb8
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-09-21 08:52:02 +0600

    Add: show-> Post date &  Author

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -30,3 +30,40 @@ function the_post_custom_thumbnail($post_id, $size = 'featured-thumbnail', $addi
 }
 
 
+function aquila_posted_on(){
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+    // Post is modified ( when post published time and modified time are different )
+
+    if(get_the_time('U') !== get_the_modified_time('U')){
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>
+        <time class="updated" datetime="%3$s">%4$s</time>';
+    } 
+
+    // Replace %1$s with datetime attribute for the time element, and %2$s with the date/time string. and %3$s with datetime attribute for the time element, and %4$s with the date/time string.
+
+    $time_string = sprintf(
+        $time_string,
+        esc_attr(get_the_date(DATE_W3C)),
+        esc_attr(get_the_date()),
+        esc_attr(get_the_modified_date(DATE_W3C)),
+        esc_attr(get_the_modified_date())
+    );
+    
+    $posted_on = sprintf(
+        esc_html_x('Posted on %s', 'post date', 'aqula'),
+        '<a href="'. esc_url(get_permalink()) .'" rel="bookmark">'. $time_string .'</a>'
+    );
+
+    echo '<span class="posted-on text-secondary">'. $posted_on .'</span>';
+
+}
+
+function aquila_posted_by(){
+    $byline = sprintf(
+        esc_html_x(' by %s', 'post author', 'aqula'),
+        '<span class="author vcard"><a href="'. esc_url(get_author_posts_url(get_the_author_meta('ID'))) .'">'. esc_html(get_the_author()) .'</a></span>'
+    );
+
+    echo '<span class="byline text-secondary">'. $byline .'</span>';
+}
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Template for post entry meta
+ * 
+ * @package Aquila
+ */
+?>
+
+<div class="entry-meta mb-3">
+    <?php
+     aquila_posted_on(); 
+     aquila_posted_by();
+     ?>
+</div>
