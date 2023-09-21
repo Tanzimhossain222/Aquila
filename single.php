0091bdf4307c48f1a6ea58f965b15f8a4ccf14af
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-09-21 12:09:45 +0600

    Add: Add pagination on Single Page

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Single Post template
  * 
--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * Template part for displaying post entry content
  * 
@@ -12,27 +13,32 @@
 
 <div class="entry-content">
     <?php
-        if(is_single()){
-            the_content(
-                sprintf(
-                    wp_kses(
-                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'aqula'),
-                        [
-                            'span' => [
-                                'class' => []
-                            ]
+    if (is_single()) {
+        the_content(
+            sprintf(
+                wp_kses(
+                    __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'aqula'),
+                    [
+                        'span' => [
+                            'class' => []
                         ]
-                    ),
-                    the_title('<span class="screen-reader-text">"', '"</span>', false)
-                )
-            );
-            
-        } else{
-            aquila_the_excerpt(200);
-           echo aquila_excerpt_more();
-
-        }
-
+                    ]
+                ),
+                the_title('<span class="screen-reader-text">"', '"</span>', false)
+            )
+        );
         
+// This  wp_link_pages() function is used to display pagination links for multi-page posts.
+    wp_link_pages(
+        [
+            'before' => '<div class="page-links">' . esc_html__('Pages:', 'aqula'),
+            'after' => '</div>',
+        ]
+    ); 
+
+    } else {
+        aquila_the_excerpt(200);
+        echo aquila_excerpt_more();
+    }
     ?>
 </div>
