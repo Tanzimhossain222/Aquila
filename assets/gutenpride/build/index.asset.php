da886d89589acee2e8c98fa032516a432bf9505d
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-09-27 10:48:46 +0600

    Add: registerBlockStyle | Gutenberg Block Variations

--- assets/gutenpride/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '9a5a056f398305f42a07');
--- assets/src/js/blocks.js
+++ assets/src/js/blocks.js
@@ -1,3 +1,6 @@
 import '../sass/blocks.scss';
-import './gutenberg/blocks/heading-with-icon/test.js';
+import './gutenberg/blocks/heading-with-icon/';
 // import './gutenberg/blocks/dos-and-donts';
+
+//blocks extensions
+import './gutenberg/block-extensions/register-block-style';
