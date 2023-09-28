6e44f7e5aa5055819b1a99045eb643188f8a2aa7
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-09-28 20:35:38 +0600

    Add: Create a Custom Archives Page

--- template-parts/content.php
+++ template-parts/content.php
@@ -4,10 +4,12 @@
  * 
  * @package Aquila
  */
+
+ $container_class = !empty($args['container_class']) ? $args['container_class'] : 'mb-5';   //This is comes from archive.php
 ?>
 
 
-<article id="post-<?php the_ID() ?>"  <?php post_class('mb-5'); ?> >
+<article id="post-<?php the_ID() ?>"  <?php post_class( $container_class); ?> >
 
 <?php 
     get_template_part('template-parts/components/blog/entry-header');
